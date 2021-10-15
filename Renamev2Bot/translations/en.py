8626b736ba84b74185b49accb1c671e59341d10f
Zylern <47510180+Zylern@users.noreply.github.com>
2021-10-15 10:19:07 +0530

    Add files via upload

--- Renamev2Bot/translations/en.py
+++ Renamev2Bot/translations/en.py
@@ -2,7 +2,7 @@ class EnTrans:
     
     WRONG_VALUE_ERROR = "**Invalid value entered for {} variable.**"
     START_MSG = "**Hey there send me a file to process.**"
-    CANCEL_MESSAGE = "**The rename has been canceled. Will be updated soon.**"
+    CANCEL_MESSAGE = "The rename has been canceled."
     RENAME_NO_FILTER_MATCH = "**NO FILTER MATCHED ABORTING RENAME \nUsing the filters to Rename. As no name was specified. Manage your Filters using /filters.**"
     RENAME_FILTER_MATCH_USED = "**Using the filters to Rename. As no name was specified. Manage your Filters using /filters**"
     RENAME_NOFLTR_NONAME = "**Enter the rename file name in format :**\n `/rename my new filename.extension` or `Use /filters to set some rename filters.`"
